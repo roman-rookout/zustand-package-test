bd0f89746878ebb226ca68be027704ebce4f9f49
roman <roman.borodov@dynatrace.com>
2024-12-29 18:57:41 +0200

    components

--- packages/component-lib/src/store.ts
+++ packages/component-lib/src/store.ts
@@ -1,34 +1,43 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+// Debugging code to check useState
+console.log('React version from3:', React.version);
+console.log('useState from3:', useState);
+console.log('setting react3');
+(window as any).React3 = React;
+
 
 const useStore = () => {
-    // Step 2: Initialize state
-    const [state, setState] = useState({
-      // Add your initial state properties here
-      count: 0,
-    });
-  
-    // Step 3: Create functions to update the state
-    const increment = () => {
-      setState(prevState => ({
-        ...prevState,
-        count: prevState.count + 1,
-      }));
-    };
+  // Step 2: Initialize state
+  console.log('React version from inside:', React.version);
+  console.log('useState from inside:', useState);
+  const [state, setState] = useState({
+    // Add your initial state properties here
+    count: 0,
+  });
 
-    const decrement = () => {
-        setState(prevState => ({
-            ...prevState,
-            count: prevState.count - 1,
-        }));
-        }
-  
-
-    // Step 4: Return the state and the functions
-    return {
-      state,
-      increment,
-      decrement
-    };
+  // Step 3: Create functions to update the state
+  const increment = () => {
+    setState(prevState => ({
+      ...prevState,
+      count: prevState.count + 1,
+    }));
   };
-  
-  export default useStore;
+
+  const decrement = () => {
+    setState(prevState => ({
+      ...prevState,
+      count: prevState.count - 1,
+    }));
+  }
+
+
+  // Step 4: Return the state and the functions
+  return {
+    state,
+    increment,
+    decrement
+  };
+};
+
+export default useStore;
