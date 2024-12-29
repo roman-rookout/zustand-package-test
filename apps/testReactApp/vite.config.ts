5c1282ab4f8cf356d76064b64e98a95f92193fea
roman <roman.borodov@dynatrace.com>
2024-12-29 18:57:46 +0200

    d

--- apps/testReactApp/vite.config.ts
+++ apps/testReactApp/vite.config.ts
@@ -1,16 +1,51 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+//import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { viteSingleFile } from "vite-plugin-singlefile"
 import path from 'path';
+//import { viteExternalsPlugin } from 'vite-plugin-externals';
+
+import { Plugin } from 'vite';
+
+export function logReactPath(): Plugin {
+  return {
+    name: 'log-react-path',
+    configResolved(config) {
+     // const reactPath = config.resolve.alias;
+      console.log('React is resolved to:', JSON.stringify(config.resolve.alias));
+    }
+  };
+}
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  resolve: {
-    alias: {
-      react: path.resolve('../../node_modules/react'),
-      'react-dom': path.resolve('../../node_modules/react-dom'),
+export default defineConfig(() => {
+  return {
+    plugins: [react(), viteSingleFile(), logReactPath(),
+      //    viteExternalsPlugin({
+      //   vue: 'Vue',
+      //   react: 'React',
+      //   'react-dom': 'ReactDOM',
+      //   // value support chain, transform to window['React']['lazy']
+      //   lazy: ['React', 'lazy']
+      // }, { disableInServe: true })
+    ],
+    rollupOptions: {
+      //external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        // globals: {
+        //   react: 'React',
+        //   'react-dom': 'ReactDOM',
+        // },
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js'
+      },
     },
-    dedupe: ["react", "react-dom"],
+    resolve: {
+      alias: {
+        react: path.resolve(__dirname, '../../node_modules/react'),
+        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
+      },
+      dedupe: ["react", "react-dom"],
+    },
   }
 })
--- packages/component-lib/vite.config.ts
+++ packages/component-lib/vite.config.ts
@@ -1,24 +1,47 @@
 import path, { resolve } from "path";
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import react from '@vitejs/plugin-react';
+//import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+
+//import { viteExternalsPlugin } from "vite-plugin-externals";
+
+import { Plugin } from 'vite';
+export function logReactPath(): Plugin {
+  return {
+    name: 'log-react-path',
+    configResolved(config) {
+     // const reactPath = config.resolve.alias;
+      console.log('React is resolved to:', JSON.stringify(config.resolve.alias));
+    }
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig((_) => {
   return {
     plugins: [
+      react(), 
       dts({
         insertTypesEntry: true
       }),
-      react()
+      logReactPath(),
+      // viteExternalsPlugin({
+      //   // vue: 'Vue',
+      //   react: 'React',
+      //   'react-dom': 'ReactDOM',
+      //   // value support chain, transform to window['React']['lazy']
+      //   lazy: ['React', 'lazy']
+      // })
     ],
     build: {
       lib: {
         entry: resolve(__dirname, 'src/index.ts'),
         fileName: 'index',
         formats: ['es'],
-        name: 'zus-test'
+        name: 'zus-test',
       },
+      minify: false,
       copyPublicDir: false
     },
     rollupOptions: {
@@ -34,9 +57,13 @@ export default defineConfig((_) => {
     },
     resolve: {
       alias: {
-        react: path.resolve('../../node_modules/react'),
-        'react-dom': path.resolve('../../node_modules/react-dom')
+        react: path.resolve(__dirname, '../../node_modules/react'),
+        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
       },
+      // alias: {
+      //   react: path.resolve('./node_modules/react'),
+      //   'react-dom': path.resolve('./node_modules/react-dom'),
+      // },
       dedupe: ['react', 'react-dom'],
     },
     // resolve: {
