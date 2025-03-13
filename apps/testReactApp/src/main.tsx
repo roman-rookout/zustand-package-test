59987e32b48a4714c0901b43a82e69054db95e8f
Doron Eli Rachman <doron.elirachman@dynatrace.com>
2025-03-13 14:39:21 +0200

    fix PR

--- apps/testReactApp/src/main.tsx
+++ apps/testReactApp/src/main.tsx
@@ -1,10 +1,8 @@
 import React, { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-console.log("React version:", React.version);
-console.log("useState:", useState);
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
