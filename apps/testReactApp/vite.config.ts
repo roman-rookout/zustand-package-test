b2135b3c74fd3d7b4502014e500f22ed83219494
Doron Eli Rachman <doron.elirachman@dynatrace.com>
2025-03-13 13:26:02 +0200

    working app

--- apps/testReactApp/vite.config.ts
+++ apps/testReactApp/vite.config.ts
@@ -1,52 +1,13 @@
 import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { viteSingleFile } from "vite-plugin-singlefile"
-import path from 'path';
-//import { viteExternalsPlugin } from 'vite-plugin-externals';
 
-import { Plugin } from 'vite';
-
-export function logReactPath(): Plugin {
-  return {
-    name: 'log-react-path',
-    configResolved(config) {
-     // const reactPath = config.resolve.alias;
-      console.log('React is resolved to:', JSON.stringify(config.resolve.alias));
-    }
-  };
-}
 
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [react(), viteSingleFile(), logReactPath(),
-      //    viteExternalsPlugin({
-      //   vue: 'Vue',
-      //   react: 'React',
-      //   'react-dom': 'ReactDOM',
-      //   // value support chain, transform to window['React']['lazy']
-      //   lazy: ['React', 'lazy']
-      // }, { disableInServe: true })
-    ],
-    optimizeDeps: {
-      include: ['@test/component-lib']
-    },
-    rollupOptions: {
-      output: {
-        // globals: {
-        //   react: 'React',
-        //   'react-dom': 'ReactDOM',
-        // },
-       // assetFileNames: 'assets/[name][extname]',
-       // entryFileNames: '[name].js',
-      },
-    },
+    plugins: [react(), viteSingleFile()],
     resolve: {
-      alias: {
-        react: path.resolve(__dirname, '../../node_modules/react'),
-        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
-      },
       dedupe: ["react", "react-dom"],
     },
   }
--- packages/component-lib/vite.config.ts
+++ packages/component-lib/vite.config.ts
@@ -1,72 +1,30 @@
 import react from '@vitejs/plugin-react';
-import path, { resolve } from "path";
+import { resolve } from "path";
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-//import react from '@vitejs/plugin-react-swc';
 
-//import { viteExternalsPlugin } from "vite-plugin-externals";
-
-import { Plugin } from 'vite';
-export function logReactPath(): Plugin {
-  return {
-    name: 'log-react-path',
-    configResolved(config) {
-      // const reactPath = config.resolve.alias;
-      console.log('React is resolved to:', JSON.stringify(config.resolve.alias));
-    }
-  };
-}
 
 // https://vitejs.dev/config/
 export default defineConfig((_) => {
   return {
     plugins: [
       react(),
-      dts({
-        insertTypesEntry: true,
-        rollupTypes: true, //DORON, PART OF LATEST COMBINA
-      }),
-      logReactPath(),
-      // viteExternalsPlugin({
-      //   // vue: 'Vue',
-      //   react: 'React',
-      //   'react-dom': 'ReactDOM',
-      //   // value support chain, transform to window['React']['lazy']
-      //   lazy: ['React', 'lazy']
-      // })
-    ],
+      dts()],
     optimizeDeps: {
       exclude: ['react', 'react-dom']
     },
     build: {
       lib: {
         entry: resolve(__dirname, 'src/index.ts'),
-        //fileName: 'index',
         formats: ['es', 'umd'], //DORON PART OF UMD COMBINA
         name: 'zus-test',
-        //TODO: FOR DORON, this is some combina which is not working but was leading somewhere
         fileName: (format) => `index.${format}.js`
       },
       minify: false,
-      copyPublicDir: false
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-        //  assetFileNames: 'assets/[name][extname]',
-        // entryFileNames: '[name].js'
-      },
-    },
-    resolve: {
-      alias: {
-        react: path.resolve(__dirname, '../../node_modules/react'),
-        'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
-      },
-      dedupe: ['react', 'react-dom'],
+      copyPublicDir: false,
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+      }
     },
   }
 })
