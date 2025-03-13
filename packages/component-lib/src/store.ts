0538060baf64889062bf00c7ad4487483c56db7d
Doron Eli Rachman <doron.elirachman@dynatrace.com>
2025-03-13 14:32:20 +0200

    fix zustand store

--- packages/component-lib/src/store.ts
+++ packages/component-lib/src/store.ts
@@ -1,43 +1,15 @@
-import React, { useState } from "react";
+import { create } from 'zustand';
 
-// Debugging code to check useState
-console.log('React version from3:', React.version);
-console.log('useState from3:', useState);
-console.log('setting react3');
-(window as any).React3 = React;
-
-
-const useStore = () => {
-  // Step 2: Initialize state
-  console.log('React version from inside:', React.version);
-  console.log('useState from inside:', useState);
-  const [state, setState] = useState({
-    // Add your initial state properties here
-    count: 0,
-  });
-
-  // Step 3: Create functions to update the state
-  const increment = () => {
-    setState(prevState => ({
-      ...prevState,
-      count: prevState.count + 1,
-    }));
-  };
-
-  const decrement = () => {
-    setState(prevState => ({
-      ...prevState,
-      count: prevState.count - 1,
-    }));
-  }
-
-
-  // Step 4: Return the state and the functions
-  return {
-    state,
-    increment,
-    decrement
-  };
+type Store = {
+    count: number;
+    increment: () => void;
+    decrement: () => void;
 };
+
+const useStore = create<Store>((set) => ({
+    count: 0,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+    decrement: () => set((state) => ({ count: state.count - 1 })),
+}));
 
 export default useStore;
